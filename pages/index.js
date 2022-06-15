7fef9661282587f6243be76a5def96d1a64d4c5a
Gaston <corbalan.gaston@outlook.com>
2022-06-15 18:18:47 -0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -64,6 +64,11 @@ export default function Home() {
           </span>
         </a>
       </footer>
+      <style jsx>{`
+      footer a {
+        color: red;
+      }
+    `}</style>
     </div>
   )
 }
